17dfab321659c94374d49596c607d2e97d5fd612
LudmilaSavchenko <ludmila.sav.96@gmail.com>
2022-12-01 23:24:11 +0300

    uodated index.html

---
+++ yatube/posts/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+
+
+# Create your views here.
+"""
+def index(request):
+    return HttpResponse('<b>Главная страница</b>')
+"""
+def index(request):
+    template = 'posts/index.html'
+    title = 'Социальная сеть блогеров Yatube'
+    context = {
+        'title': title,
+        'text': 'Это главная страница проекта Yatube'
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, pk):
+    return HttpResponse(f'Пост номер {pk}')
+
+    """
+       template = 'posts/group_list.html'
+    context = {
+        'text': 'Здесь будет информация о группах проекта Yatube'
+    }
+    return render(request, template, context) 
+    """
